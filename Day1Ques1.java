94ee3de2d97ee33c3bc5a5cefd43c7c831b7115a
Vishwas Nigam <90500935+VishwasNigam02@users.noreply.github.com>
2023-09-20 11:29:06 +0530

    Update Day1Ques1.java
    The issue was with the sc.nextLine() after sc.nextInt(). When you use nextInt(), it reads the integer value but does not consume the newline character ('\n') in the input stream. So, when you call sc.nextLine() immediately after sc.nextInt(), it reads the remaining newline character from the previous input line, which results in an empty string.

--- Day1Ques1.java
+++ Day1Ques1.java
@@ -9,8 +9,10 @@ public class StrQues {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int empno = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
         String name = sc.nextLine();
         int age = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
         String country = sc.nextLine();
         String result = "";
         result = addMethod(empno, name, age, country);
