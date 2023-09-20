067ff60f4ae49f62acfc98ebfa438afbc19654b0
Vishwas Nigam <90500935+VishwasNigam02@users.noreply.github.com>
2023-09-20 21:46:39 +0530

    Update Day1Ques3.java
    WAP that accepts a three digit number and prints it in word for eg 351 as Three Five One

--- Day1Ques3.java
+++ Day1Ques3.java
@@ -1,1 +1,49 @@
+import java.util.Scanner;
+
+public class NumName {
+    public static String name(int n) {
+        if (n == 0) {
+            return "Zero ";
+        }
+        if (n == 1) {
+            return "One ";
+        }
+        if (n == 2) {
+            return "Two ";
+        }
+        if (n == 3) {
+            return "Three ";
+        }
+        if (n == 4) {
+            return "Four ";
+        }
+        if (n == 5) {
+            return "Five ";
+        }
+        if (n == 6) {
+            return "Six ";
+        }
+        if (n == 7) {
+            return "Seven ";
+        }
+        if (n == 8) {
+            return "Eight ";
+        }
+        if (n == 9) {
+            return "Nine ";
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String res = "";
+        for (int i = 0; i < 3; i++) {
+            res = name(a % 10) + res;
+            a = a / 10;
+        }
+        System.out.println(res);
+    }
+}
 
