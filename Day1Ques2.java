8dfe74706df47071025202a86013cf39d0a748d3
Vishwas Nigam <90500935+VishwasNigam02@users.noreply.github.com>
2023-09-21 10:56:27 +0530

    Update Day1Ques2.java

--- Day1Ques2.java
+++ Day1Ques2.java
@@ -1,13 +1,21 @@
-public class SumQues {
-    public int sumMethod(int a, int b, int c) {
+import java.util.*;
+public class Main {
+    public static int sumMethod(int a, int b, int c) {
         int sum = 0;
-        if (a >= b && a <= c)
+        if (a >= b && a <= c || a >= c && a <= b)
             sum = b + c;
-        if (b >= a && b <= c)
+        if (b >= a && b <= c || b >= c && b <= a)
             sum = a + c;
-        if (c >= b && c <= a)
+        if (c >= b && c <= a || c >= a && c <= b)
             sum = b + a;
         return sum;
     }
-
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        System.out.println(sumMethod(a,b,c));
+        
+    }
 }
