fff125fb0c1697386f7796dee1bf972a339cbd7d
Vishwas Nigam <90500935+VishwasNigam02@users.noreply.github.com>
2023-09-22 17:00:15 +0530

    Create Day2Ques3.java
    still left

---
+++ Day2Ques3.java
@@ -0,0 +1,45 @@
+public class Main 
+{
+    static int productId;
+    static String productName;
+    static String description;
+    static float price;
+    static float maxDiscountAllowed;
+    static int monthOfManufacture;
+    static int yearOfManufacture;
+    static boolean imported;
+    
+    public static int getProductId()
+    {
+        return productId;
+    }
+    public static void setProductId(int productId)
+    {
+        this.productId= productId;
+    }
+    public static String getProductName()
+    {
+        return productName;
+    }
+    public static void setProductName(String productName)
+    {
+        this.productName= productName;
+    }
+    public static String getDescription()
+    {
+        return description;
+    }
+    public static void setDescription(String description)
+    {
+        this.description= description;
+    }
+    public static float getPrice()
+    {
+        return price;
+    }
+    public static void setPrice(float price)
+    {
+        this.price= price;
+    }
+    
+}
